59d6d9d8fff76f55a42d834655d5f30482c55b2a
Čerňanský Aleš <ales.cernansky@unicorn.com>
2018-08-08 16:49:11 +0200

    Refactoring to submodules

---
+++ sfg-pet-clinic-model/src/main/java/com/cernana/springcourse/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.cernana.springcourse.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ sfg-pet-clinic-model/src/main/java/com/cernana/springcourse/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.cernana.springcourse.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
