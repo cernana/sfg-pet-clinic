bb4f4b9fd59dd5f66e5f983dbaa1cba8b0f723ca
Čerňanský Aleš <ales.cernansky@unicorn.com>
2018-08-09 08:50:24 +0200

    Added base entity to all persisted entities.

--- sfg-pet-clinic-model/src/main/java/com/cernana/springcourse/sfgpetclinic/model/Person.java
+++ sfg-pet-clinic-model/src/main/java/com/cernana/springcourse/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.cernana.springcourse.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 	
 	private String firstName;
 	
--- sfg-pet-clinic-model/src/main/java/com/cernana/springcourse/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic-model/src/main/java/com/cernana/springcourse/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.cernana.springcourse.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 	
 	private String name;
 
